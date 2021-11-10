5cdd129f1d006a03ce0333c313adc8fe32126eba
wuyanzhen <florence590@163.com>
2021-11-10 14:30:38 +0800

    添加拦截器

--- src/main/java/com/florence/resources/config/WebMvcConfig.java
+++ src/main/java/com/florence/resources/config/WebMvcConfig.java
@@ -2,12 +2,14 @@ package com.florence.resources.config;
 
 import com.florence.resources.interceptors.RightToViewInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author wuyanzhen
  */
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Autowired
